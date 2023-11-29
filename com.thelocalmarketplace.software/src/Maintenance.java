29e30cd29e2a1686b743742a746f147fd84e469c
Kelly <kelly.osena@yahoo.com>
2023-11-28 18:29:27 -0700

    linked hardware listener to Maintenance

--- com.thelocalmarketplace.software/src/Maintenance.java
+++ com.thelocalmarketplace.software/src/Maintenance.java
@@ -1,4 +1,8 @@
-public class Maintenance {
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
+import com.jjjwelectronics.printer.ReceiptPrinterListener;
+
+public class Maintenance implements ReceiptPrinterListener {
     private Software software;
     public Maintenance(Software software){
         this.software = software;
@@ -28,4 +32,64 @@ public class Maintenance {
     // may need different return type
     public void needBanknotes(){
     }
+
+	@Override
+	public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void thePrinterIsOutOfPaper() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void thePrinterIsOutOfInk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void thePrinterHasLowInk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void thePrinterHasLowPaper() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void paperHasBeenAddedToThePrinter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inkHasBeenAddedToThePrinter() {
+		// TODO Auto-generated method stub
+		
+	}
 }
