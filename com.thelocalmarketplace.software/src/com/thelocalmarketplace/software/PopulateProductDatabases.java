edfdaa8ab146533caedadfe039133e6b1a8b142d
kayle <kayle@kayleexps>
2023-12-04 14:13:28 -0700

    fixed duplicate items

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PopulateProductDatabases.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PopulateProductDatabases.java
@@ -1,29 +1,3 @@
- /**
- *Project, Iteration 3, Group 4
- *  Group Members:
- * - Arvin Bolbolanardestani / 30165484
- * - Anthony Chan / 30174703
- * - Marvellous Chukwukelu / 30197270
- * - Farida Elogueil / 30171114
- * - Ahmed Elshabasi / 30188386
- * - Shawn Hanlon / 10021510
- * - Steven Huang / 30145866
- * - Nada Mohamed / 30183972
- * - Jon Mulyk / 30093143
- * - Althea Non / 30172442
- * - Minori Olguin / 30035923
- * - Kelly Osena / 30074352
- * - Muhib Qureshi / 30076351
- * - Sofia Rubio / 30113733
- * - Muzammil Saleem / 30180889
- * - Steven Susorov / 30197973
- * - Lydia Swiegers / 30174059
- * - Elizabeth Szentmiklossy / 30165216
- * - Anthony Tolentino / 30081427
- * - Johnny Tran / 30140472
- * - Kaylee Xiao / 30173778 
- **/
-
 package com.thelocalmarketplace.software;
 
 import com.jjjwelectronics.Numeral;
@@ -45,11 +19,15 @@ public class PopulateProductDatabases {
 		   
 		   PriceLookUpCode plu = generateRandomPLU();
 		   PLUCodedProduct pluProduct = new PLUCodedProduct(plu, randomPLUProduct(), generateRandomPrice());
-		   ProductDatabases.PLU_PRODUCT_DATABASE.put(plu, pluProduct);
+		   if (!ProductDatabases.PLU_PRODUCT_DATABASE.containsKey(plu)) {
+			   ProductDatabases.PLU_PRODUCT_DATABASE.put(plu, pluProduct);
+		   }
+		   
 		   Barcode barcode = generateRandomBarcode();
 		   BarcodedProduct barcodedProduct = new BarcodedProduct(barcode, randomBarcodedProduct(), generateRandomPrice(), generateRandomMass());
-		   ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode, barcodedProduct);
-		   
+		   if (!ProductDatabases.BARCODED_PRODUCT_DATABASE.containsKey(barcode)) {
+			   ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode, barcodedProduct);
+		   }
 
 		   System.out.print(onesCounter);
 		   System.out.println(tensCounter);
@@ -106,6 +84,9 @@ public class PopulateProductDatabases {
 		if (onesCounter == 10) {
 			onesCounter = 0;
 			tensCounter++;
+		} else if (tensCounter == 2 && onesCounter == 5) {
+			onesCounter = 0;
+			tensCounter = 0;
 		}
 	}
 	
